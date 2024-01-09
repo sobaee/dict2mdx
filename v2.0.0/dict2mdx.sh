6b02930ffa7582ec9f28b4231cf9ca2ea7c2d1a3
sobaee <65910585+sobaee@users.noreply.github.com>
2024-01-09 15:57:26 +0300

    Add files via upload

--- v2.0.0/dict2mdx.sh
+++ v2.0.0/dict2mdx.sh
@@ -12,13 +12,13 @@
 if command -v python3 >/dev/null 2>&1; then
     if command -v pyglossary >/dev/null 2>&1; then
         if command -v mdict >/dev/null 2>&1; then
-            echo -e "\n If you want to convert any dict to MDX, then convert it to .mtxt first, and the script will ask you the next step!\n\n"
+            echo -e "All dependings are ready!!\n"
         else
             echo "ERROR: mdict not found! Run 'pip3 install mdict-utils'!"
             exit 1
         fi
     else
-        echo "ERROR: pyglossary not installed! Run 'pip3 install pyglossary'"
+        echo "ERROR: pyglossary not installed! Install my modified version as you read on github"
         exit 1
     fi
 else
@@ -26,7 +26,46 @@ else
     exit 1
 fi
 
-python main.py --cmd
+read -p "If your file is already .mtxt and/or sources folder and you want to convert directly to MDX and/or MDD press (y)!! OR press any other key if not! " answer1
+    case $answer1 in
+    y|Y) 
+    src=""
+    read -p "Enter the .mtxt dict name: " src
+
+    printf ${src%.*} > description.html
+    printf ${src%.*} > title.html
+    if [ -e "${src%.*}.mtxt" ]; then
+       mdict --title title.html --description description.html -a "${src%.*}.mtxt" "${src%.*}.mdx"
+
+        if [ -d "${src%.*}.mtxt_res" ]; then
+        mdict -a "${src%.*}.mtxt_res" "${src%.*}.mdd"
+        echo -e ' Sources is also converted to MDD \n '
+        fi
+
+        
+        echo 'All done!'
+        exit 1
+    else
+        echo -e "ERROR: ${src%.*}.mtxt doesn't found\n"
+        if [ -d "${src%.*}.mtxt_res" ]; then
+        mdict -a "${src%.*}.mtxt_res" "${src%.*}.mdd"
+        echo -e '\n Only MDD is packed!!!'
+        fi
+        exit 1
+    fi
+
+        ;;
+    n|N) 
+        echo -e ' Your conversion will continue \n '
+       
+        ;;
+    *) # Invalid choice
+        echo -e ' Your conversion will continue \n '
+        ;;
+esac
+
+
+pyglossary --cmd
 
 echo
 echo
@@ -43,6 +82,7 @@ read -p "Convert .mtxt to MDX? (y) or press any other key to exit? " answer
 
         if [ -d "${src%.*}.mtxt_res" ]; then
         mdict -a "${src%.*}.mtxt_res" "${src%.*}.mdd"
+        echo -e ' Sources is also converted to MDD \n '
         fi
 
         
